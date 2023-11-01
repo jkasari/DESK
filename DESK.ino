5ef58a47bd241b7c5a2ceb7e0725f056b6185a7c
Jason <jkasari@outlook.com>
2023-10-31 20:53:28 -0700

    Clean up the loop -create on and off functions for the lights

--- DESK.ino
+++ DESK.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+#include <EEPROM.h>
 
 #define LEFT_STRAND_PIN 6 // The pin that controls the strand on the left of the desk
 #define LEFT_LED_COUNT 32 // Number of leds on the left
@@ -29,8 +30,8 @@ void setup() {
     rightStrip.begin();
     rightStrip.clear();
     for (int i = 0; i < TOTAL_LED_COUNT; ++i) {
-        dots[i].offSetRed = 4;
-        dots[i].offSetGreen = 4;
+        dots[i].offSetRed = 2;
+        dots[i].offSetGreen = 2;
         dots[i].offSetBlue = 1;
     }
     pinMode(WRENCH_SWITCH, INPUT_PULLUP);
@@ -39,37 +40,9 @@ void setup() {
 
 void loop() {
     if (buttonReadLow(WRENCH_SWITCH) == true) {
-        wasOn = true;
-        while (mainBrightness < 255*(TOTAL_LED_COUNT)) {
-            int i = 0;
-            int32_t temp = mainBrightness;
-            while (temp > 0) {
-                if (temp > 255) {
-                    dots[i].brightness = 255;
-                } else {
-                    dots[i].brightness = mainBrightness%255;
-                }
-                paintDot(dots[i], i);
-                temp -= 255;
-                i++;
-            }
-            showStrips();
-            mainBrightness+=20;
-            delay(1);
-        }
+        turnOn();
     } else {
-        if (wasOn) {
-            for (int i = 255; i >= 0; --i) {
-                for (int j = 0; j < TOTAL_LED_COUNT; ++j) {
-                    dots[j].brightness = i;
-                    paintDot(dots[j], j);
-                }
-                showStrips();
-                delay(5);
-            }
-            mainBrightness  = 0;
-        }
-        wasOn = false;
+        turnOff();
     }
 
 }
@@ -97,4 +70,40 @@ bool buttonReadLow(uint8_t pin) {
         }
     }
     return false;
+}
+
+void turnOn(void) {
+    wasOn = true;
+    while (mainBrightness < 255*(TOTAL_LED_COUNT)) {
+        int i = 0;
+        int32_t temp = mainBrightness;
+        while (temp > 0) {
+            if (temp > 255) {
+                dots[i].brightness = 255;
+            } else {
+                dots[i].brightness = mainBrightness%255;
+            }
+            paintDot(dots[i], i);
+            temp -= 255;
+            i++;
+        }
+        showStrips();
+        mainBrightness+=20;
+        delay(1);
+    }
+}
+
+void turnOff(void) {
+    if (wasOn) {
+        for (int i = 255; i >= 0; --i) {
+            for (int j = 0; j < TOTAL_LED_COUNT; ++j) {
+                dots[j].brightness = i;
+                paintDot(dots[j], j);
+            }
+            showStrips();
+            delay(5);
+        }
+        mainBrightness  = 0;
+    }
+    wasOn = false;
 }
